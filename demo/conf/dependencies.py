c9d50b5882fb3d561759e3fbafd8bcc5d332d0d1
Olexiy Strashko <olexiy.strashko@gmail.com>
2011-07-30 21:37:58 +0300

    + improve dependencies for the future (libs, apps, static)

--- demo/conf/dependencies.py
+++ demo/conf/dependencies.py
@@ -1,13 +1,22 @@
+import sigurd
 
-class Dependencies(object):
-    EXT_APPS = "apps/ext/"
-    EXT_LIBS = "ext_libs"
+# @TODO: UNIMPLEMENTED CODE FOR DJANGO DASH (FUTURE IDEA)
 
-    def application_deps(self):
-        self.require_app("admin-tools", version="0.2", config_only=True, config_id="dashboard_only")
-        self.require_app("admin-tools", version="0.2", config_only=True)
-        self.require_app("haystack", version="3.1")
+class Dependencies(sigurd.BaseDependencies):
+    EXT_APPS_PATH = "apps/ext/"
+    EXT_LIBS_PATH = "ext_libs/"
+    EXT_STATIC_PATH = "static/ext/"
 
-    def lib_deps(self):
-        self.require_lib("python-twitter")
-        self.require_lib("whoosh", "0.2")
+    def resolve_apps(self):
+        self.resolve_app("admin-tools", version="0.2", config_only=True, config_id="dashboard_only")
+        self.resolve_app("admin-tools", version="0.2", config_only=True)
+        self.resolve_app("haystack", version="3.1")
+
+    def resolve_libs(self):
+        self.resolve_lib("python-twitter")
+        self.resolve_lib("whoosh", "0.2")
+
+    def resolve_statics(self):
+        self.resolve_static("jquery", version="1.6.1")
+        self.resolve_static("fancybox", version="0.1")
+
