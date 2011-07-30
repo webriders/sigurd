8764e029393995a75d85ad2ec7ea99f2c20ea98a
Andrey Sokolov <andrey.sokolov@webriders.com.ua>
2011-07-30 21:09:31 +0400

    add top menu add 'app config add' page

--- hive/source/apps/web/views.py
+++ hive/source/apps/web/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from web.models import Application
+from web.forms import ApplicationConfigForm
 
 
 class HomeView(TemplateView):
@@ -13,10 +14,20 @@ class ApplicationsList(ListView):
     context_object_name = 'applications_list'
     queryset = Application.objects.filter(published=True)
 
-    def get_context_data(self, **kwargs):
-        context = super(ApplicationsList, self).get_context_data(**kwargs)
+applications_list = ApplicationsList.as_view()
 
+
+class AppConfigDetail(DetailView):
+    template_name = 'pages/app_config_detail.html'
+
+    def get_object(self, queryset=None):
+        return None
+    
+    def get_context_data(self, **kwargs):
+        context = super(AppConfigDetail, self).get_context_data(**kwargs)
+
+        context['app_config_form'] = ApplicationConfigForm()
 
         return context
 
-applications_list = ApplicationsList.as_view()
+add_app_config = AppConfigDetail.as_view()
