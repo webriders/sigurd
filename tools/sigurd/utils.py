115090eba8f5890b04ad2c00a5c0351b08e8cadf
Olexiy Strashko <olexiy.strashko@gmail.com>
2011-07-31 13:27:16 +0300

    + class by str path method added

--- tools/sigurd/utils.py
+++ tools/sigurd/utils.py
@@ -2,6 +2,7 @@
 
 def get_class_by_path( kls ):
     '''
+    Get class object by python path string
     See: http://stackoverflow.com/questions/452969/does-python-have-an-equivalent-to-java-class-forname
     '''
     parts = kls.split('.')
