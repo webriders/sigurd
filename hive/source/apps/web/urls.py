5dd460d21288119bb789409ecd2bdeed22f5af1b
Andrey Sokolov <andrey.sokolov@webriders.com.ua>
2011-07-31 17:33:23 +0400

    update urls

--- hive/source/apps/web/urls.py
+++ hive/source/apps/web/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('web.views',
     url(r'^$', 'home', name='home'),
-    url(r'^applications/$', 'applications_list', name='applications_list'),
-    url(r'^applications/config/add/$', 'add_app_config', name='add_app_config'),
-    url(r'^applications/config/add/success/$', 'add_app_config_success', name='add_app_config_success'),
-    url(r'^applications/(?P<app_slug>[-\w]+)/download/$', 'config_downloader', name='config_downloader'),
-    url(r'^applications/(?P<app_slug>[-\w]+)/(?P<config_slug>[-\w]+)/download/$', 'config_downloader', name='config_downloader'),
+    url(r'^configs/$', 'applications_list', name='applications_list'),
+    url(r'^configs/add/$', 'add_app_config', name='add_app_config'),
+    url(r'^configs/add/success/$', 'add_app_config_success', name='add_app_config_success'),
+    url(r'^configs/(?P<app_slug>[-\w]+)/download/$', 'config_downloader', name='config_downloader'),
+    url(r'^configs/(?P<app_slug>[-\w]+)/(?P<config_slug>[-\w]+)/download/$', 'config_downloader', name='config_downloader'),
 )
