dd2e54c2966dbe48dabdb16e42b7481c0353a97e
Andrey Sokolov <andrey.sokolov@webriders.com.ua>
2011-07-31 13:44:27 +0400

    rename field 'file_name' to 'archive'

--- hive/source/apps/web/forms.py
+++ hive/source/apps/web/forms.py
@@ -5,10 +5,10 @@ class ApplicationConfigForm(forms.ModelForm):
     class Meta:
         model = ApplicationConfig
         fields = ('application', 'title', 'description', 'slug', 'supported_versions',
-                  'file_name', 'author_name', 'author_email', )
+                  'archive', 'author_name', 'author_email', )
 
     def get_archive_ext(self):
-        archive = self.cleaned_data['file_name']
+        archive = self.cleaned_data['archive']
         extensions = ('.tar.gz', '.tgz')
 
         for ext in extensions:
@@ -16,12 +16,12 @@ class ApplicationConfigForm(forms.ModelForm):
                 return ext
         return None
 
-    def clean_file_name(self):
+    def clean_archive(self):
         correct_ext  = self.get_archive_ext()
         if not correct_ext:
             raise forms.ValidationError("Archive must be in one of such formats: " + ', '.join(extensions))
 
-        return self.cleaned_data['file_name']
+        return self.cleaned_data['archive']
 
     def save(self, commit=True):
         app_slug = self.cleaned_data['application'].slug
@@ -30,6 +30,6 @@ class ApplicationConfigForm(forms.ModelForm):
 
         name = "(%(app_slug)s)__%(config_slug)s.%(extension)s"\
              % dict( app_slug=app_slug, config_slug=config_slug, extension=extension)
-        self.instance.file_name.name = name
+        self.instance.archive.name = name
 
         return super(ApplicationConfigForm, self).save(commit)
