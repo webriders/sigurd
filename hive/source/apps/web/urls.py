65504664b304b98d58d649b0938f92db1845f1a8
Andrey Sokolov <andrey.sokolov@webriders.com.ua>
2011-07-31 11:53:25 +0400

    add top menu add 'app config add' page

--- hive/source/apps/web/urls.py
+++ hive/source/apps/web/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('web.views',
     url(r'^$', 'home', name='home'),
     url(r'^applications/$', 'applications_list', name='applications_list'),
     url(r'^applications/config/add/$', 'add_app_config', name='add_app_config'),
+    url(r'^applications/config/add/success/$', 'add_app_config_success', name='add_app_config_success'),
 )
